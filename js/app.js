bbbbdf601810d55f2db1afb9a45c7a42c5badddd
ayoub ar <ayoubarahmat9@gmail.com>
2021-02-24 20:57:40 +0100

    add Weather class

---
+++ js/app.js
@@ -0,0 +1,2 @@
+const form = document.getElementById('form-all');
+const inputCity = document.getElementById('input');
---
+++ js/weather.js
@@ -0,0 +1,19 @@
+class Weather {
+  constructor() {
+    this.token = 'd8678ba3deee99a2bb7db9670c8c9713';
+    this.url = 'api.openweathermap.org/data/2.5/weather';
+  }
+
+  // crée une method qui retourne toutes les donées d'une ville
+  get_data_by_city(city) {
+      /*
+            URL : api.openweathermap.org/data/2.5/weather?q=paris&appid=b6d02d1eb8eeb0eecdb765455078822e
+      */
+    const config= {
+        method: "GET"
+    }
+    const response = await fetch(`${this.url}/weather?q=${city}&appid=${this.token}`, config)
+
+    return response.json();
+  }
+}
